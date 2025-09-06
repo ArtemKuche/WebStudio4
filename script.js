48a02d9a61bc167fd6d4952c1c42c88271c1e6b0
Artkurch <173730056a@gmail.com>
2025-09-06 10:26:16 -0500

    modal-finish

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("modal");
-  const openModalBtn = document.getElementById("openModalBtn");
-  const closeModalBtn = document.getElementById("closeModal");
-
-  openModalBtn.onclick = () => {
-    modal.style.display = "block";
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  closeModalBtn.onclick = () => {
-    modal.style.display = "none";
-  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  window.onclick = (e) => {
-    if (e.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-});
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
